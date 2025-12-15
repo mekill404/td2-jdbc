1790522c788395967dab5fcd54c2a6591c5ab696
mekill404 <hei.mahery.55@gmail.com>
2025-12-15 11:53:00 +0100

    feat(db): Initialize mini_football_db and dedicated manager user
    This commit introduces the foundational SQL script (`db.sql`) to set up the application's database environment.
    
    It performs the following key setup steps:
    1. Creates the `mini_football_db` database.
    2. Creates the `mini_football_db_manager` user, responsible for database operations.
    3. Grants the `mini_football_db_manager` user the necessary privileges (CREATE, SELECT, INSERT, UPDATE, DELETE) to manage tables and perform all CRUD operations within the new database.

---
+++ target/classes/com/mekill404/data/db.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS mini_football_db;
+CREATE DATABASE mini_football_db;
+
+CREATE USER mini_football_db_manager WITH PASSWORD 'miniFootBall123';
+
+\connect mini_football_db;
+
+GRANT CONNECT ON DATABASE mini_football_db TO mini_football_db_manager;
+
+GRANT USAGE ON SCHEMA public TO mini_football_db_manager;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO mini_football_db_manager;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO mini_football_db_manager;
