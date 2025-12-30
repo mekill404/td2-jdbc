10ef1cb0cf566cdf815e955c1f1a83c05f7e02b0
mekill404 <hei.mahery.55@gmail.com>
2025-12-30 15:48:21 +0100

    feat(data): insert data in db

--- src/main/java/com/mekill404/data/data.sql
+++ src/main/java/com/mekill404/data/data.sql
@@ -1,13 +1,15 @@
-INSERT INTO Team (name, continent) VALUES
-('Real Madrid CF', 'EUROPA'),
-('FC Barcelona', 'EUROPA'),
-('Atletico de Madrid', 'EUROPA'),
-('Al Ahly SC', 'AFRICA'),
-('Inter Miami CF', 'AMERICA');
+TRUNCATE TABLE Player, Team RESTART IDENTITY CASCADE;
 
-INSERT INTO Player (name, age, position, team_id) VALUES
-('Thibaut Courtois', 32, 'GK', 1),
-('Dani Carvajal', 33, 'DEF', 1),
-('Jude Bellingham', 21, 'MIDF', 1),
-('Robert Lewandowski', 36, 'STR', 2),
-('Antoine Griezmann', 33, 'STR', 3);
+INSERT INTO Team (name, continent) VALUES 
+    ('Real Madrid CF', 'EUROPA'),
+    ('FC Barcelona', 'EUROPA'),
+    ('Atletico de Madrid', 'EUROPA'),
+    ('Al Ahly SC', 'AFRICA'),
+    ('Inter Miami CF', 'AMERICA');
+
+INSERT INTO Player (name, age, position, id_team) VALUES 
+    ('Thibaut Courtois', 32, 'GK', 1),
+    ('Dani Carvajal', 33, 'DEF', 1),
+    ('Jude Bellingham', 21, 'MIDF', 1),
+    ('Robert Lewandowski', 36, 'STR', 2),
+    ('Antoine Griezmann', 33, 'STR', 3);
--- src/main/java/com/mekill404/data/schema.sql
+++ src/main/java/com/mekill404/data/schema.sql
@@ -10,6 +10,7 @@ CREATE TYPE Continent AS ENUM (
     'EUROPA',
     'ASIA',
     'AMERICA',
+    'OCEANIA'
 )
 CREATE TABLE Team(
     id SERIAL PRIMARY KEY,
@@ -22,7 +23,7 @@ CREATE TABLE Player(
     name VARCHAR(255) NOT NULL,
     age INT NOT NULL CHECK (age BETWEEN 15 AND 45),
     position Position NOT NULL DEFAULT 'GK',
-    team_id INT NOT NULL,
+    id_team INT,
     CONSTRAINT fk_player_team FOREIGN KEY (team_id) REFERENCES Team(id) ON DELETE RESTRICT
 )
 
